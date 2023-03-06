89d02d280ad21c5d2161068ec87697460d9e62c4
Kjw19 <63536145+Kjw19@users.noreply.github.com>
2023-03-06 17:34:53 +0900

    type: style
    body: 채팅상세 css변경

--- together/src/main/resources/static/js/message.js
+++ together/src/main/resources/static/js/message.js
@@ -72,8 +72,8 @@ $(function(){
 								//타인 메시지
 								output += '<div class="chat-message-left">';
 								output += '<div class="space-photo">';
-								output += '<img src="../mypage/photoView.do?mem_num='+item.mem_num+'" width="40" height="40" class="my-photo">';
 								output += '</div><div class="message-header">';
+								output += '<img src="../mypage/photoView.do?mem_num='+item.mem_num+'" width="40" height="40" class="my-photo">';
 								output += '<div class="username">';
 								output += item.mem_name;
 								output += '</div>';
